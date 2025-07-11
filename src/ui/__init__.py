2ea66a7c5672e07f5a1437acf3aa484aed2b3ccc
Nhimphu <dexter088013@gmail.com>
2025-07-11 13:01:49 +0300

    Add QuickABTestWizard UI

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,4 +1,5 @@
 from .filters_panel import FiltersPanel
 from .settings import SettingsWidget
+from .wizard import QuickABTestWizard
 
-__all__ = ["FiltersPanel", "SettingsWidget"]
+__all__ = ["FiltersPanel", "SettingsWidget", "QuickABTestWizard"]
